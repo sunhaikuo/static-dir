3986a2eac717819bdc1b76161d09ba145295a51c
孙海阔 <sunhaikuo@meituan.com>
2020-08-11 22:40:26 +0800

    0721

--- README.md
+++ README.md
@@ -1,14 +1,31 @@
-# 在当前目录启动一个 Web Server
- * 使用方法
+# Start a Web Server for your current directory or relative directory
+
+## Installation
+---
 
 ```
 npm install -g static-dir
-
-cd TO_SERVER_DIR
-
-static
-
 ```
 
- 工具会自动当前目录做为web服务器，自动打开浏览器，地址为：http://127.0.0.1:10010
+## Usage
+---
+
+* current directory
+
+```
+cd TO_YOUR_DIR
+
+static
+```
+
+* relative directory
+
+```
+static www
+
+note: make sure the 'www' folder is exit, else it's will work on your PWD directory*
+```
+
+* finally it's will open your webbrowser, the address is : http://127.0.0.1:10010
+
 
--- index.js
+++ index.js
@@ -2,13 +2,31 @@
 
 const express = require("express");
 const open = require("open");
-
+const path = require('path')
+const fs = require('fs-extra')
 const app = express();
 const port = 10010;
-const path = process.cwd()
+const curPath = process.cwd()
+const color = require('colors')
 
-app.use(express.static(path));
+let dirPath = ''
+
+const argv = process.argv
+if (argv.length > 2) {
+  const newPath = path.resolve(curPath, argv[2])
+  const isExit = fs.existsSync(newPath)
+  if (isExit) {
+    dirPath = newPath
+  } else {
+    console.log(`WARN: ${newPath} is not exit! \npath will use ${curPath}`.yellow)
+    dirPath = curPath
+  }
+} else {
+  dirPath = curPath
+}
+
+app.use(express.static(dirPath));
 app.listen(port, () => {
-  console.log("当前运行的目录：", path);
+  console.log(`work path is ${dirPath}`.green);
   open("http://127.0.0.1:10010");
 });
