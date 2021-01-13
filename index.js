0e329af55682b81126467df181c2ba7a8ff43e34
孙海阔 <sunhaikuo@meituan.com>
2021-01-13 17:08:01 +0800

    feat: 增加端口占用

--- index.js
+++ index.js
@@ -1,32 +1,49 @@
 #! /usr/bin/env node
 
-const express = require("express");
-const open = require("open");
-const path = require('path')
-const fs = require('fs-extra')
+const express = require('express');
+const open = require('open');
+const path = require('path');
+const fs = require('fs-extra');
 const app = express();
-const port = 10010;
-const curPath = process.cwd()
-const color = require('colors')
+let port = 10010;
+const curPath = process.cwd();
+const color = require('colors');
+const { exec } = require('child_process');
 
-let dirPath = ''
+let dirPath = '';
 
-const argv = process.argv
+const argv = process.argv;
 if (argv.length > 2) {
-  const newPath = path.resolve(curPath, argv[2])
-  const isExit = fs.existsSync(newPath)
-  if (isExit) {
-    dirPath = newPath
-  } else {
-    console.log(`WARN: ${newPath} is not exit! \npath will use ${curPath}`.yellow)
-    dirPath = curPath
-  }
+    const newPath = path.resolve(curPath, argv[2]);
+    const isExit = fs.existsSync(newPath);
+    if (isExit) {
+        dirPath = newPath;
+    } else {
+        console.log(`WARN: ${newPath} is not exit! \npath will use ${curPath}`.yellow);
+        dirPath = curPath;
+    }
 } else {
-  dirPath = curPath
+    dirPath = curPath;
 }
 
-app.use(express.static(dirPath));
-app.listen(port, () => {
-  console.log(`work path is ${dirPath}`.green);
-  open("http://127.0.0.1:10010");
-});
+function checkPort() {
+    exec(`lsof -i tcp:${port}`, (error, std) => {
+        if (std.trim() == '') {
+            console.log(`port ${port} is avaliable!`);
+        } else {
+            console.log(`port ${port} is in used!`);
+            port++;
+            checkPort();
+        }
+    });
+}
+
+checkPort();
+
+setTimeout(() => {
+    app.use(express.static(dirPath));
+    app.listen(port, () => {
+        console.log(`work path is ${dirPath}`.green);
+        open(`http://127.0.0.1:${port}`);
+    });
+}, 2000);
